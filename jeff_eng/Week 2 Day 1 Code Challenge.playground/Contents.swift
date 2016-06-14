bee58ed3c5e11e0f649b59edb4f110a4a944b1cd
Jeff Eng <jeff@jeffeng.me>
2016-06-13 21:55:45 -0700

    finished the reverse array function using simple for in loop

--- jeff_eng/Week 2 Day 1 Code Challenge.playground/Contents.swift
+++ jeff_eng/Week 2 Day 1 Code Challenge.playground/Contents.swift
@@ -1,9 +1,26 @@
-//Week 2 Day 1 Assignment
+//Week 2 Day 1 Assignment Code Challenge
 
 import UIKit
 
 //Write a function that reverses an array
 
-var array = ["Apple", "Google", "Facebook", "LinkedIn"]
+var appleHardware = ["iPhone", "iPad", "iPod", "MacBook", "Apple Watch", "Apple TV"]
+//Result of array needs to look like: ["Apple TV", "Apple Watch", "MacBook", "iPod", "iPad", "iPhone"]
+
+
+func reverseArray(inputArray: [String]) -> [String] {
+    //Initialize an empty array of strings
+    var outputArray = [String]()
+    
+    //Loop through the array of strings
+    for item in inputArray.count.predecessor().stride(through: 0, by: -1) {
+        outputArray.append(inputArray[item])
+    }
+    //Return the completed array
+    return outputArray
+}
+
+//Testing the function reverseArray by passing in the appleHardware array of strings
+reverseArray(appleHardware)
 
 
